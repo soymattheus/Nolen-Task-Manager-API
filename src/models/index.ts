66e1dd83ff3b7a84a3f81d6f650043743ca8cb7f
Matheus Tavares <matheustavares1a@gmail.com>
2026-01-30 19:51:19 -0300

    feature/table assossiation

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,4 +1,17 @@
 import User from "./user";
 import TokenBlacklist from "./tokenBlacklist";
+import Task from "./task";
 
-export { User, TokenBlacklist };
+User.hasMany(Task, {
+  foreignKey: "idUser",
+  sourceKey: "idUser",
+  as: "tasks",
+});
+
+Task.belongsTo(User, {
+  foreignKey: "idUser",
+  targetKey: "idUser",
+  as: "user",
+});
+
+export { User, TokenBlacklist, Task };
